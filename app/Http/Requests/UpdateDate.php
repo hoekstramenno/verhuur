266056f78d800c0fbd7fef91ab2a5b02ee5a90d2
Menno Hoekstra <menno@digitalvillage.nl>
2017-08-05 13:18:58 +0200

    API for dates is done with CRUD

---
+++ app/Http/Requests/UpdateDate.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDate extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'date_from' => 'required|date|date_format:Y-m-d H:i:s|after:tomorrow',
+            'date_to' => 'required|date|date_format:Y-m-d H:i:s|after:date_from',
+            'published_at' => 'date|date_format:Y-m-d H:i:s',
+            'price' => 'required|numeric',
+            'status' => 'required'
+        ];
+    }
+}
